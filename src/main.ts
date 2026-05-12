97ce8ebff259c421875a74dbe277b7c1001aca48
mkiuzzal01 <mkiuzzal07@gmail.com>
2026-05-12 16:03:51 +0600

    intially setup the crud application

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  console.log(`Application is running on: ${PORT}`);
+}
+
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
---
+++ src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+
+  findAll() {
+    return {
+      message: 'All users',
+      data: [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'demo@gmail.com',
+        },
+      ],
+    };
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return updateUserDto;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
